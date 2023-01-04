f5734af3c765deca3d9865fb636b81b90cbc3549
mikhail.zavidov <mikezav68@gmail.com>
2023-01-04 10:35:26 +0300

    initial

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,10 @@ public class Main {
         task1();
         task2();
     }
-
     public static void task1 () {
         System.out.println("Задача 1");
         // Пишем код для задачи 1
     }
-
     public static void task2 () {
         System.out.println("Задача 2");
         // Пишем код для задачи 2
